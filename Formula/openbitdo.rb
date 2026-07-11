5d84000526116d964d07857feb436acd4f307f3e
Brook <carcleaningbrosmaa@gmail.com>
2026-07-11 02:31:26 +0000

    Update openbitdo formula

--- Formula/openbitdo.rb
+++ Formula/openbitdo.rb
@@ -2,22 +2,22 @@ class Openbitdo < Formula
   desc "Beginner-first clean-room 8BitDo TUI utility"
   homepage "https://github.com/bybrooklyn/openbitdo"
   license "BSD-3-Clause"
-  version "0.0.1-rc.3"
+  version "0.0.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1-rc.3/openbitdo-v0.0.1-rc.3-macos-arm64.tar.gz"
-      sha256 "c2ec96eb6a40b7b748ccee873030fac446d94db29cf7b20858b3ecc8d83bcb25"
+      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1/openbitdo-v0.0.1-macos-arm64.tar.gz"
+      sha256 "80e61ccf7a3b1db57de22f74979fabb77239d6bf56884514d70b9743398532bf"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1-rc.3/openbitdo-v0.0.1-rc.3-linux-x86_64.tar.gz"
-      sha256 "fa7ae583796dc979a64eaeb670a06fc7c10fd51bbb12fce5815d66629e18c9a0"
+      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1/openbitdo-v0.0.1-linux-x86_64.tar.gz"
+      sha256 "025cc82e9638c9feb46e9c029cb85bbb4f7e5bb36757fc3f9b32ba5942096f1f"
     elsif Hardware::CPU.arm?
-      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1-rc.3/openbitdo-v0.0.1-rc.3-linux-aarch64.tar.gz"
-      sha256 "992586ff89da6dfdb137fd424f651ccedb4a65de0f43d615cda682e31e222dd0"
+      url "https://github.com/bybrooklyn/openbitdo/releases/download/v0.0.1/openbitdo-v0.0.1-linux-aarch64.tar.gz"
+      sha256 "6aa5c9244c393a85f00a4dd0acdf9210f88a5c53baaed2dfea91d1f9b434ca9a"
     end
   end
 
